a5f244e803f8c966791c3f8b33c5040486600960
Lucas Borges <lucasnsborges@gmail.com>
2019-10-15 12:02:26 -0300

    feat: add static header and currency balance view

---
+++ src/pages/Home/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar, Image, Text } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{uri: 'https://66.media.tumblr.com/edade13f43a8da4c675ca3e59c4d853b/tumblr_pn5jmut8xi1wwb8qz_1280.jpg'}}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 10, fontWeight: 'bold', color: '#FFF' }}>TOTAL BALANCE</Text>
+      </View>
+      <View style={styles.imageContainer}>
+        <FontAwesome5 name="bell" size={18} color="#FFF" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 64,
+    marginTop: StatusBar.currentHeight,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  imageContainer: {
+    padding: 20
+  },
+
+  image: {
+    borderRadius: 32,
+    width: 32,
+    height: 32,
+  }
+});
