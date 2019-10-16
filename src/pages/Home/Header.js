a4ab7d4ac2cf368336ca94e8b54a34dd86d66e48
Lucas Borges <lucasnsborges@gmail.com>
2019-10-16 03:05:01 -0300

    feat: update Home header

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -2,22 +2,33 @@ import React, { PureComponent } from 'react';
 import { View, StyleSheet, StatusBar, Image, Text } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
+import TouchableItem from '../../components/TouchableItem';
+
 export default class Header extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.imageContainer}>
+        <TouchableItem
+          borderless={true}
+          style={styles.wrapper}
+        >
           <Image
-            style={styles.image}
+            style={styles.wrapper}
             source={{uri: 'https://66.media.tumblr.com/edade13f43a8da4c675ca3e59c4d853b/tumblr_pn5jmut8xi1wwb8qz_1280.jpg'}}
           />
-        </View>
+        </TouchableItem>
+
         <View>
           <Text style={{ fontSize: 10, fontWeight: 'bold', color: '#FFF' }}>TOTAL BALANCE</Text>
         </View>
-        <View style={styles.imageContainer}>
-          <FontAwesome5 onPress={() => this.props.navigation.navigate('Notifications')} name="bell" size={18} color="#FFF" />
-        </View>
+
+        <TouchableItem
+          borderless={true}
+          style={styles.wrapper}
+          // onPress={() => this.props.navigation.navigate('Notifications')}
+        >
+          <FontAwesome5 name="bell" size={18} color="#FFF" />
+        </TouchableItem>
       </View>
     );
   }
@@ -26,19 +37,18 @@ export default class Header extends PureComponent {
 const styles = StyleSheet.create({
   container: {
     height: 64,
+    padding: 16,
     marginTop: StatusBar.currentHeight,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
   },
 
-  imageContainer: {
-    padding: 20
-  },
-
-  image: {
+  wrapper: {
     borderRadius: 32,
-    width: 32,
     height: 32,
-  }
+    width: 32,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
